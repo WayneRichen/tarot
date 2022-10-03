33b3568ce858b3c1f71c637e4732b0c93959d02e
Wayne <wayne@ocard.co>
2022-10-03 22:41:13 +0800

    Adjust title to text-3xl

--- resources/js/Layouts/Background.vue
+++ resources/js/Layouts/Background.vue
@@ -7,7 +7,7 @@ import image from '../../assets/background.png';
     <div class="relative bg-gray-900">
         <div class="relative mx-auto max-w-md h-[85vh] overflow-hidden bg-no-repeat"
             :style="{backgroundImage: 'url(' + image +')'}">
-            <div class="text-center text-5xl font-extrabold py-4">
+            <div class="text-center text-4xl font-extrabold py-4">
                 <Link href="/" class="bg-clip-text text-transparent bg-gradient-to-t from-orange-50 to-orange-200 hover:from-orange-200 hover:to-orange-50">
                     塔羅解惑
                 </Link>
